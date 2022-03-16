0b1ad0053f273b7d227e01957eeda1c39b4591c7
Darius <tarvydasdarius@gmail.com>
2022-03-16 08:44:58 +0100

    BackEnd

---
+++ chatapp-backend/src/chats/dtos/update-chat.dto.ts
@@ -0,0 +1,6 @@
+import {CreateChatDto} from "./create-chat.dto";
+import {PartialType} from "@nestjs/mapped-types";
+
+export class UpdateChatDto extends PartialType(CreateChatDto){
+    id: number;
+}
